02cf10275a0635547d7647ee758792cd9e935c1e
yutaokoshi <ookoshiyuuta@gmail.com>
2021-05-21 02:01:58 +0900

    Location変更

--- item.py
+++ item.py
@@ -1,10 +1,11 @@
 
 from distutils.util import strtobool
 import main
-import datetime
+from datetime import datetime, timedelta, timezone
 import re
 from time import mktime
 import env
+
 
 class Item:
 
@@ -12,13 +13,13 @@ class Item:
 
         self.id = item["guid"]
         self.asin = item["guid"][0:10].strip()
-        self.published = datetime.datetime.fromtimestamp(
-            mktime(item["published_parsed"]))
+        self.published = datetime.fromtimestamp(
+            mktime(item["published_parsed"]), timezone(timedelta(hours=+9), 'JST'))
         self.affUrl = "https://www.amazon.co.jp/dp/" + \
             self.asin+"/ref=nosim?tag=" + env.AFF_TAG
 
-        # 文字数制限280文字なのでそれを超えないように大体100文字にtitleを丸める
-        self.title = item["title"][0:100]
+        # 文字数制限140文字なのでそれを超えないように大体40文字にtitleを丸める
+        self.title = item["title"][0:50]
         # A列がASIN記入欄という前提で[0]にしている
         affList = [r[0].strip() for r in main.AFFList]
 
@@ -30,8 +31,8 @@ class Item:
             if re.match(
                 r'[0-9]{4}\.[0-9]{2}\.[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}',
                     main.AFFList[self.columnNumber][3]):
-                self.lastUpdate = datetime.datetime.strptime(
-                    main.AFFList[self.columnNumber][3], '%Y.%m.%d %H:%M:%S')
+                self.lastUpdate = datetime.strptime(
+                    main.AFFList[self.columnNumber][3]+'+0900', '%Y.%m.%d %H:%M:%S%z')
             else:
                 self.lastUpdate = None
 
@@ -59,8 +60,9 @@ class Item:
 ■商品名：{title}
 ■URL：{url}
 ［{date}]
-'''.format(title=self.title, url=self.affUrl, date=self.published).strip()
-
+'''.format(title=self.title, url=self.affUrl, date=self.published.strftime('%Y.%m.%d %H:%M:%S')).strip()
+        print(text)
+        print(len(text))
         main.TweetApi.update_status(text)
         main.AFFSheet.update_cell(
             self.columnNumber+3,  # 先頭行2行削除した分を1から始まるので+3
